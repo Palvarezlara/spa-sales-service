892f93b3efb777b35dcc17ec0045d355d62e902b
PalvarezLara <pame.alvarez@duocuc.cl>
2025-12-02 05:26:12 -0300

    corsConfig

--- src/main/java/cl/duoc/spa/spa_sales_service/config/CorsConfig.java
+++ src/main/java/cl/duoc/spa/spa_sales_service/config/CorsConfig.java
@@ -1,5 +1,5 @@
 package cl.duoc.spa.spa_sales_service.config;
-
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -20,3 +20,4 @@ public class CorsConfig {
         };
     }
 }
+*/
